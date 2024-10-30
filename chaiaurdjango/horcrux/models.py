8a7565e3816f8f19cfebd06da2eb8d27e0466354
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-10-30 08:02:27 +0530

    migrations

--- chaiaurdjango/horcrux/models.py
+++ chaiaurdjango/horcrux/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class ChaiVariety(models.Model):
+    CHAI_TYPE_CHOICE = [
+        ('ML', 'MASALA'),
+        ('GR', 'GINGER'),
+        ('PL', 'PLAIN'),
+        ('KL', 'KIWI'),
+        ('EL', 'ELAICHI')
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='horcrux/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE)

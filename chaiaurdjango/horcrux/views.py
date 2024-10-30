fc11ceea5fdc7633bc896d2772d0a7d9ab6d0c6b
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-10-30 08:25:50 +0530

    images in frontend

--- chaiaurdjango/horcrux/views.py
+++ chaiaurdjango/horcrux/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import ChaiVariety
 
 # Create your views here.
 
 def all_horcrux(request):
-    return render(request, 'horcrux/all_items.html')
+    chais = ChaiVariety.objects.all()
+    return render(request, 'horcrux/all_items.html', {'chais' : chais})
 
 def about_them(request):
     return render(request, 'horcrux/about.html')
